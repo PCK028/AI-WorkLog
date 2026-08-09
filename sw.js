35ca49dda5562efbf42abcbebdcd95bb0a138a5a
PCK028 <ckprajwal@gmail.com>
2026-08-09 18:19:12 +0530

    V5.3.9 verified Gemini 3.1 Flash-Lite

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'worklog-v538-v1';
+const CACHE = 'worklog-v539-v1';
 const SHELL = ['./','./index.html','./manifest.json','./sw.js','./xlsx-writer.js'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
